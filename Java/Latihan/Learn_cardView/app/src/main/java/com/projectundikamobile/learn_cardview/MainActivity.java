b839d601cb26242c9e6131ccdc913d38cf4373bf
Fadil <fadilfdl2710@gmail.com>
2021-08-19 17:19:52 +0700

    Update Learn setOnClick CardView

--- Java/Latihan/Learn_cardView/app/src/main/java/com/projectundikamobile/learn_cardview/MainActivity.java
+++ Java/Latihan/Learn_cardView/app/src/main/java/com/projectundikamobile/learn_cardview/MainActivity.java
@@ -7,9 +7,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     protected CardView cv_first, cv_second;
+
+    public void initial() {
+
+        cv_first = (CardView) findViewById(R.id.cv_first);
+        cv_second = (CardView) findViewById(R.id.cv_second);
+
+        cv_first.setOnClickListener(this);
+        cv_second.setOnClickListener(this);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,36 +27,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initial();
-        setOnClick();
 
     }
 
-    public void initial() {
 
-        cv_first = (CardView) findViewById(R.id.cv_first);
-        cv_second = (CardView) findViewById(R.id.cv_second);
+    @Override
+    public void onClick(View v) {
 
-    }
+        Intent intent;
 
-    public void setOnClick () {
+        switch (v.getId()) {
 
-        cv_first.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            case R.id.cv_first:
 
-                Intent intent = new Intent(getApplicationContext(), cardView.class);
+                intent = new Intent(this, tes.class);
                 startActivity(intent);
+                break;
 
-            }
-        });
+            case R.id.cv_second:
 
-        cv_second.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                intent = new Intent(this, cardView.class);
+                startActivity(intent);
+                break;
 
-            }
-        });
+        }
 
     }
-
 }
---
+++ Java/Latihan/Learn_cardView/app/src/main/java/com/projectundikamobile/learn_cardview/tes.java
@@ -0,0 +1,14 @@
+package com.projectundikamobile.learn_cardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tes);
+    }
+}
