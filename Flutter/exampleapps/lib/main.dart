63f81f219aac4484894356a75aea6b018806b405
Fadil <fadilfdl2710@gmail.com>
2022-04-02 19:13:32 +0700

    Update

--- Flutter/exampleapps/lib/main.dart
+++ Flutter/exampleapps/lib/main.dart
@@ -4,37 +4,69 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  var colorPink = Colors.pinkAccent;
+  var colorGreenStabilo = Colors.lightGreenAccent;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Testing Flutter :)",
       home: Scaffold(
-          appBar: AppBar(title: Text("heyy, Mr.Morning Star !!wkowkok")),
+          appBar: AppBar(
+              title: Text("heyy, Mr.Morning Star !!wkowkok"),
+            backgroundColor: Colors.pinkAccent,
+          ),
           body: Container(
+            // margin: EdgeInsets.symmetric(vertical: 50.0),
+              padding: EdgeInsets.symmetric(vertical: 50.0),
               // color: Colors.pink,
+              color: Colors.black12,
               child: Column(
                 children: [
-                  Row(
-                    children: [
-                      Text("Anak Muda Indonesia"),
-                      Text("Wow, Hello Mr.Morning Star!!!")
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Card(
+                  Container(
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
 
-                        color: Colors.pink,
-                        child: Row(
                           children: [
-                            Icon(Icons.star_border_outlined)
+                            Text("Anak Muda Indonesia"),
+                            Text("Wow, Hello Mr.Morning Star!!!")
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Card(
+                              //Margin
+                                margin: EdgeInsets.symmetric(horizontal: 20.0),
+                                color: Colors.pink,
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.star_border_outlined)
+                                  ],
+                                )
+                            ),
+                            Card(
+                                margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                                color: colorGreenStabilo,
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.star_border_purple500),
+
+                                  ],
+                                )
+                            )
                           ],
                         )
-                      ),
-                      Card(
-                        child: Text("Haiii"),
-                      )
-                    ],
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 50.0),
+                    color: colorPink,
+                    child: Text("test"),
                   )
                 ],
               ))),
