51dbc8469976ab794d9026296e4c7473a4647976
Fadil <fadilfdl2710@gmail.com>
2022-04-03 15:19:05 +0700

    Update Images

--- Flutter/exampleapps/lib/main.dart
+++ Flutter/exampleapps/lib/main.dart
@@ -4,77 +4,66 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   var colorPink = Colors.pinkAccent;
   var colorGreenStabilo = Colors.lightGreenAccent;
 
-
   //Button
   var button1 = Container(
-        width: 200,
-        height: 45,
-        margin: EdgeInsets.symmetric(vertical: 50.0),
-        child: TextButton(
-            style: TextButton.styleFrom(
-              backgroundColor: Colors.pinkAccent,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20)
-              ),
-            ),
-            onPressed: () {},
-            child: Text(
-              "Button Testing",
-              style: TextStyle(
-                color: Colors.white70,
-
-              ),
-            )
-        )
-    );
-  var button2 = Container(
       width: 200,
       height: 45,
+      margin: EdgeInsets.symmetric(vertical: 50.0),
       child: TextButton(
-        style: TextButton.styleFrom(
-          backgroundColor: Color(0xffF18265),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.pinkAccent,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
           ),
-        ),
-        onPressed: () {},
-        child: Text(
-          "Let's take a picture",
-          style: TextStyle(
-            color: Color(0xffffffff),
-          ),
+          onPressed: () {},
+          child: Text(
+            "Button Testing",
+            style: TextStyle(
+              color: Colors.white70,
+            ),
+          )));
+  var button2 = Container(
+    width: 200,
+    height: 45,
+    child: TextButton(
+      style: TextButton.styleFrom(
+        backgroundColor: Color(0xffF18265),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
         ),
       ),
-    );
+      onPressed: () {},
+      child: Text(
+        "Let's take a picture",
+        style: TextStyle(
+          color: Color(0xffffffff),
+        ),
+      ),
+    ),
+  );
 
   //========================
 
   //Card
   var _card1 = Card(
       //Margin
-        margin: EdgeInsets.symmetric(horizontal: 20.0),
-        color: Colors.pink,
-        child: Row(
-          children: [
-            Icon(Icons.star_border_outlined)
-          ],
-        )
-    );
+      margin: EdgeInsets.symmetric(horizontal: 20.0),
+      color: Colors.pink,
+      child: Row(
+        children: [Icon(Icons.star_border_outlined)],
+      ));
 
   var _card2 = Card(
-        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-        color: Colors.lightGreenAccent,
-        child: Row(
-          children: [
-            Icon(Icons.star_border_purple500),
-
-          ],
-        )
-    );
+      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+      color: Colors.lightGreenAccent,
+      child: Row(
+        children: [
+          Icon(Icons.star_border_purple500),
+        ],
+      ));
 
   //========================
 
@@ -84,12 +73,11 @@ class MyApp extends StatelessWidget {
       title: "Testing Flutter :)",
       home: Scaffold(
           appBar: AppBar(
-              title: Text("Tetsing Success !!!"),
+            title: Text("Tetsing Success !!!"),
             backgroundColor: Colors.pinkAccent,
           ),
-          body:
-          Container(
-            // margin: EdgeInsets.symmetric(vertical: 50.0),
+          body: Container(
+              // margin: EdgeInsets.symmetric(vertical: 50.0),
               padding: EdgeInsets.symmetric(vertical: 50.0),
               // color: Colors.pink,
               color: Colors.black12,
@@ -114,31 +102,25 @@ class MyApp extends StatelessWidget {
                         //Row Card
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            _card1,
-                            _card2,
-                            // _image
-                            Container(
-                              child: Image.asset("assets/image/we.png"),
-                            )
-
-                          ],
+                          children: [_card1, _card2],
                         )
                       ],
                     ),
                   ),
                   button1,
                   button2,
-
                   Container(
-
+                    child: Image.asset(
+                      "assets/image/we.png",
+                      height: 200,
+                      width: 200,
+                      ),
                   )
                 ],
               ))),
     );
   }
 }
-
 
 class RandomWord extends StatefulWidget {
   @override
