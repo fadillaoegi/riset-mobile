9eed5627a43095d31e49dd3d7141cb2e4afb7df8
Fadil <fadilfdl2710@gmail.com>
2022-04-07 13:28:21 +0700

    Update

--- Flutter/examplechatting/lib/main.dart
+++ Flutter/examplechatting/lib/main.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
+import 'pages/Chatting.dart';
 
+void main() => runApp(chatting());
+
+class chatting extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
+      title: "Chatting",
+      home: Chatting(),
+    );
+  }
+}
---
+++ Flutter/examplechatting/lib/pages/Chatting.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Chatting extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: SafeArea(
+          child: ListView(
+        children: [
+          Text(
+            "Hello Mr.Morning Start !!",
+            style: TextStyle(fontSize: 40),
+          ),
+          Row(children: [
+            new Icon(
+              Icons.abc_outlined,
+              size: 100,
+            )
+          ])
+        ],
+      )),
+    );
+  }
+}
