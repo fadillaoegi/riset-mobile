aaf14859068da151023f3f25bb705931b4e74b72
Fadil <fadilfdl2710@gmail.com>
2022-04-08 21:26:18 +0700

    SetUp Break

---
+++ Flutter/examplechatting/lib/widgets/style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color endingNV = Color(0xff182C61);
+Color shipOffier = Color(0xff2C3A47);
+Color lynxWhite = Color(0xfff5f6fa);
+
+TextStyle title = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+
+TextStyle subTitle = TextStyle(fontSize: 20.0);
