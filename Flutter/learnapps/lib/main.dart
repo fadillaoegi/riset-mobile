d9d6a156ff40044c93cb78bbc3955d1cc2eda81a
Fadil <fadilfdl2710@gmail.com>
2022-04-15 15:28:46 +0700

    Update

--- Flutter/learnapps/lib/main.dart
+++ Flutter/learnapps/lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(learn());
 }
 
-class learn extends StatelessWidget{
+class learn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("data", style: GoogleFonts.poppins()),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
   }
 }
