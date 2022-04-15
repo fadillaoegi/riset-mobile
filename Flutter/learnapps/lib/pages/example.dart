0aae57cc07c1e9db86bc212dd123f170f2d7c6c7
Fadil <fadilfdl2710@gmail.com>
2022-04-15 16:59:52 +0700

    Break buka puasa

--- Flutter/learnapps/lib/pages/example.dart
+++ Flutter/learnapps/lib/pages/example.dart
@@ -13,20 +13,69 @@ class example extends StatelessWidget {
         // child: Center(
         child: SingleChildScrollView(
           child: Column(
-            // mainAxisAlignment: MainAxisAlignment.start,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 60,
-              ),
-              Image.asset(
-                "assets/images/item1.png",
-                height: 140,
-                width: 140,
-              ),
-            ],
-          ),
+              // mainAxisAlignment: MainAxisAlignment.start,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      "assets/images/item1.png",
+                      height: 140,
+                      width: 140,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Image.asset(
+                      "assets/images/item2.png",
+                      height: 100,
+                      width: 100,
+                    ),
+                    Image.asset(
+                      "assets/images/item2.png",
+                      height: 100,
+                      width: 100,
+                    ),
+                    Image.asset(
+                      "assets/images/item2.png",
+                      height: 100,
+                      width: 100,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Image.asset(
+                        "assets/images/item2.png",
+                        height: 100,
+                        width: 100,
+                      ),
+                      Image.asset(
+                        "assets/images/item2.png",
+                        height: 100,
+                        width: 100,
+                      ),
+                      Image.asset(
+                        "assets/images/item2.png",
+                        height: 100,
+                        width: 100,
+                      ),
+                    ])
+              ]),
         ),
       ),
     );
